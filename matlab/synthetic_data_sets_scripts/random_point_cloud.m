d4f51d90dd601eb45c05ade2b74eefc7302b2415
n-otter <otter@maths.ox.ac.uk>
2016-04-30 12:44:54 +0100

    minor changes
    changed description of function, output type

--- matlab/synthetic_data_sets_scripts/random_point_cloud.m
+++ matlab/synthetic_data_sets_scripts/random_point_cloud.m
@@ -1,16 +1,15 @@
-function [ output_args ] = random_point_cloud( N ,d)
-%This function outputs N points sampled uniformly at random from [0,1]^d
-% 
+function [ point_cloud ] = random_point_cloud( N ,d)
+% Sample N points uniformly at random from [0,1]^d
 %   
-%Input: N number of points 
+%Input: 
+% N number of points 
 % d dimension
 %
-%Output: text file with on each line the coordinates of a points 
+%Output: name of text file storing on each line the coordinates of a point.
 
 % Nina Otter, Oxford August 2015
 
 P=rand(N,d)
-
 
 point_cloud=['random_point_cloud_',num2str(N),'_',num2str(d),'_.txt']
 fileID=fopen(point_cloud,'w');
