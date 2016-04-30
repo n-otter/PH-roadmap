061e75fd906e60882dd6f5696f580b0c53824036
n-otter <otter@maths.ox.ac.uk>
2016-04-30 09:45:18 +0100

    Update plot_barcodes.m
    Added code to plot arrowheads for infinite bars, fixed/added some comments

--- matlab/plot_barcodes.m
+++ matlab/plot_barcodes.m
@@ -1,7 +1,7 @@
-function [] = plot_barcodes( filename,header )
+function [plot_name] = plot_barcodes( filename,header )
 % Input: text file with persistence diagram for
 % a given homological dimension, one birth death pair per line
-% Output: plot of barcodes
+% Output: pdf file with plot of barcodes
 %
 % Nina Otter, Oxford September 2015.
 
@@ -22,52 +22,52 @@ if (minb == maxd)
     maxd = maxd+1;
 end
 
-
+%The following piece of code counts the number of times an interval occurrs
 [rows,indices,indices]=unique(intervals,'rows');
-
-occurrence=accumarray(indices,1)
-%disp([occurrence,rows]);
-size(occurrence)
-size(rows)
-%new_interv=(occurrence>0);
-%intervals;
-%intervals=intervals(new_interv);
-%occurrence=occurrence(new_interv);
+occurrence=accumarray(indices,1);
 
 %Order by increasing births
-rows=sortrows(rows)
+rows=sortrows(rows);
 N=size(rows,1);
-h=figure
+h=figure;
 j=1;
-for i=1:N
-    a=rows(i,1)
-    b=rows(i,2)
+for i=1:N %loop over all intervals
+    a=rows(i,1);
+    b=rows(i,2);
     m=occurrence(i);
-    if b ~= -1
-    S=ceil(b-a)+1;
+    if b ~= -1 %First we plot the finite intervals
+    S=ceil(b-a)+2;
     x=linspace(a,b,S);
     for k=j:m+j-1;
-    y=k*ones(1,S)
-    plot(x,y,'b', 'LineWidth',3); 
-     axis([0 maxd-minb+1 0 sum(occurrence)]);
+    y=k*ones(1,S);
+    plot(x,y,'b', 'LineWidth',5); 
+     axis([0 maxd 0 sum(occurrence)]);
      hold on;
     end
     hold on;
-    else S=ceil(maxd-a)
-        x=linspace(a,maxd+20,S*100)
+    else S=ceil(maxd-a)+2; %Now we plot the infinite intervals
+        maxd
+        x=linspace(a,maxd,S)
         for k=j:m+j-1;
-            y=k*ones(1,S*100)
-        plot(x,y,'b','LineWidth',2);
-         %axis([0 25 0 15]);
-        %hold on;
-    end
-   
+            y=k*ones(1,S);
+            plot(x,y,'b','LineWidth',5);
+            hold on;
+            outside_1=plot([maxd+0.05,maxd-0.05],[k,k+0.4],'b','LineWidth',5);
+            hold on
+            outside_2=plot([maxd+0.05,maxd-0.05],[k,k-0.4],'b','LineWidth',5);
+            hold on
+            outside_3=plot([maxd,maxd+0.05],[k,k],'b','LineWidth',5);
+            hold on
+            set(outside_1,'Clipping','off');
+            set(outside_2,'Clipping','off');
+            set(outside_3,'Clipping','off');
+        end
     end
  j=j+m;
  hold on;
 end
 
-%Create plot
+%Label plot and x axis
 title(header);
 xlabel('filtration step');
 
